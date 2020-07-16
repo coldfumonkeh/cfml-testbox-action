b4db005d53323189cf28e2e91e8cb317e6505a78
Matt Gifford <me@mattgifford.co.uk>
2020-07-16 15:21:35 +0300

    Testbox

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/sh -l
 
-echo "Hello $1"
-time=$(date)
-echo "::set-output name=time::$time"
+echo "Starting the build"
+
+echo "Your CFML engine of choice is $1"
+
+curl --location -o /tmp/box.zip https://www.ortussolutions.com/parent/download/commandbox/type/bin
+unzip /tmp/box.zip -d /tmp/
+chmod a+x /tmp/box
+/tmp/box install production=false
+/tmp/box server start cfengine=$1
+mkdir /tmp/results/
+/tmp/box testbox run outputFile=testbox.xml reporter=junit
