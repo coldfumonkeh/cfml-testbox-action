b9ba451de73753e363fe0348ddbec0119732864e
Matt Gifford <me@mattgifford.co.uk>
2020-07-16 15:26:51 +0300

    Typo fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ echo "Starting the build"
 
 echo "Your CFML engine of choice is $1"
 
-box nstall production=false
+box install production=false
 box server start cfengine=$1
 mkdir /tmp/results/
 /tmp/box box testbox run outputFile=testbox.xml reporter=junit
